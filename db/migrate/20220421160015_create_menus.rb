7e1bf075437442306cd61c134e5f7dde6e09aeb7
nrlheni <nurulakhni12@gmail.com>
2022-04-21 16:10:37 +0000

    Generate Scaffold: Menu

---
+++ db/migrate/20220421160015_create_menus.rb
@@ -0,0 +1,11 @@
+class CreateMenus < ActiveRecord::Migration[7.0]
+  def change
+    create_table :menus do |t|
+      t.string :name
+      t.float :price
+      t.string :description, limit => 150
+
+      t.timestamps
+    end
+  end
+end
