e1fe2ccac88fd98ac5e4157fadae9efd91e1e288
nrlheni <nurulakhni12@gmail.com>
2022-04-22 18:05:04 +0000

    Create validation for email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   has_many :orders 
+
+  validates :email, format: { with: /\A(\S+)@(.+)\.(\S+)\z/, message: "Email invalid"  }, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
 end
