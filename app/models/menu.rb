054da9793bf8a734c9c4009323cae92cec4f2c20
nrlheni <nurulakhni12@gmail.com>
2022-04-22 16:25:15 +0000

    Add validation for name, price, description

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
   has_many :category_menus
   has_many :categories, through: :category_menus
+
+  validates :name, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :description, length: { maximum: 150 }
 end
