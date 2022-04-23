dade1b0d7116691dbd8ed3c1e720ce64571d67c0
nrlheni <nurulakhni12@gmail.com>
2022-04-23 11:34:22 +0000

    Update spec customer factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :customer do
     name { Faker::Name.name }
-    email { "nurul@gmail.com" }
+    email { "nurulakhni12@gmail.com" }
     address { Faker::Address.full_address }
     phone { "08112233" }
   end
